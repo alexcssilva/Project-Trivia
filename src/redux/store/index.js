3aa1d178015b4f9339c9a7727480bb4819d11fd8
Tiago Garbi Baganha <tgbmateriais@gmail.com>
2022-02-14 17:35:01 -0300

    requisito4

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -1,6 +1,6 @@
 import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
 import login from '../reducers/login';
 
 const store = createStore(
